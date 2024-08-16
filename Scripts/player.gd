1075950d7b0c26a6b12564d2dd99861968aeb077
addyng <110417388+addyng@users.noreply.github.com>
2024-08-16 15:48:53 -0700

    added knife animation and scene

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,19 +2,28 @@ extends CharacterBody2D
 
 @export var speed : int = 100
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+var direction = Vector2.ZERO
+var animatedDirection: String = "Down"
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func _physics_process(_delta):
-	velocity = Input.get_vector("left", "right", "up", "down") * speed
+	direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * speed
 	
+	if velocity.length() != 0:
+		animatedDirection = "down"
+		if velocity.x < 0: animatedDirection = "left"
+		elif velocity.x > 0: animatedDirection = "right"
+		elif velocity.y < 0: animatedDirection = "up"
+			
 	if velocity.x > 0:
 		sprite.flip_h = false
 	elif velocity.x < 0:
@@ -26,4 +35,8 @@ func _physics_process(_delta):
 		sprite.play("Run")
 		
 	move_and_slide()
-	
+	swingWeapon()
+
+func swingWeapon():
+	if Input.is_action_just_pressed("attack"):
+		animation_player.play("attack" + animatedDirection)
