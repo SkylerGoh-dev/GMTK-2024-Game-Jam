2db5df71bde1cff6aa025a58eeccbe4f3badaf74
addyng <110417388+addyng@users.noreply.github.com>
2024-08-17 19:26:06 -0700

    cows flip sprite to face player, updated animation

--- Scripts/cow.gd
+++ Scripts/cow.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @onready var charge: Timer = $Charge
 @onready var attack_time: Timer = $AttackTime
 @onready var beef = preload("res://Scenes/beef.tscn")
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 var speed = 50
 var direction = Vector2.ZERO
@@ -24,14 +25,20 @@ func _process(delta: float) -> void:
 func _physics_process(delta):
 	if target:
 		velocity = direction * speed
+	if global_position[0] - target.position[0] > 0:
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
 	move_and_slide()
 
 func _on_charge_timeout() -> void:
 	attack_time.start()
+	sprite.play("default")
 	speed = 0
 
 func _on_attack_time_timeout() -> void:
 	direction = position.direction_to(target.position)
+	sprite.play("charge")
 	randie = randf_range(-0.3, 0.3)
 	direction[0] += randie
 	direction[1] += randie
